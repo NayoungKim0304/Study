c7638fd72fcd66f7ae54d56208ffebdaf84a4a40
KimNayoung <kny8451@naver.com>
2022-01-07 23:50:30 +0900

    피라미드 별찍기 이해하기

--- 2022.01/src/s1.java
+++ 2022.01/src/s1.java
@@ -24,13 +24,41 @@ public class s1 {
 			// *****
 			//*******
 			
-			for(int i=0; i<4; i++) {
-				for(int j=3; j>=0; j--) {
-					System.out.println(" ");
+//			for(int i=0; i<4; i++) {
+//				for(int j=3; j>=0; j--) {
+//					System.out.println(" ");
+//				}
+//				System.out.println("*");
+//			}
+//			//--->실패..더 생각해보기
+			
+			
+			//	  *
+			//	 ***
+			//  *****
+			// *******
+			//*********
+			// 첫째줄 : 공백4칸 별1개
+			// 둘째줄 : 공백3칸 별3개
+			// 셋째줄 : 공백2칸 별5개
+			// 넷째줄 : 공백1칸 별7개
+			//다섯째줄 : 공백0칸 별9개
+			
+			//전체 줄 수(5줄이므로 i<5[위에서n=5초기화]) -> for문안에 줄바꿈출력(
+			for(int i=0; i<n; i++) {
+				//별 앞의 공백개수 4-3-2-1-0개 
+				//i=0일때 공백 4칸 따라서 j<n-i 대신 j=1부터 
+				for(int j=1; j<n-i; j++) {
+					System.out.print(" ");
 				}
-				System.out.println("*");
+				
+				//별 개수 1-3-5-7-9 
+				//i=0일때 별 1개 따라서 k<i*2+1
+				for(int k=0; k<i*2+1; k++) {
+					System.out.print("*");
+				}
+				System.out.println();
 			}
-			//--->실패..더 생각해보기
 		}
 
 }
