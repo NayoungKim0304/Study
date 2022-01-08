90285a5a9b29df3e7b0287223c6fd6918ef69799
KimNayoung <kny8451@naver.com>
2022-01-08 23:40:52 +0900

    별찍기 또 연습

--- 2022.01/src/s1.java
+++ 2022.01/src/s1.java
@@ -59,6 +59,29 @@ public class s1 {
 				}
 				System.out.println();
 			}
+			
+			//    *
+			//   **
+			//  ***
+			// ****
+			//*****
+			//연습
+			
+			//전체줄 수 n (여기서 5)
+			for(int i=0; i<n; i++) {
+				//공백 4-3-2-1-0
+				for(int j=1; j<n-i; j++) {
+					System.out.print(" ");
+				}
+				
+				//별 1-2-3-4-5
+				for(int j=0; j<i+1; j++) {
+					System.out.print("*");
+				}
+				
+				System.out.println();
+			}
+			
 		}
 
 }
