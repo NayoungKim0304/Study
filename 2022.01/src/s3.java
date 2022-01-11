eaf13ccb4fbad8a62dc4c240e02c7862b1602415
KimNayoung <kny8451@naver.com>
2022-01-11 23:55:46 +0900

    백준 2562번

---
+++ 2022.01/src/s3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class s3 {
+
+	public static void main(String[] args) {
+
+		//백준 2562번(https://www.acmicpc.net/problem/2562)
+		// 9개의 서로 다른 자연수가 주어질때 최대값을 찾고
+		// 최대값이 몇번째 수인지 구하기
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] a = new int[9];
+		
+		for(int i=0; i<a.length; i++) {
+			a[i]= sc.nextInt();
+				
+		}
+		
+		int max = a[0];
+		int index=0;
+		
+		for(int i=0; i<a.length; i++) {
+			if(max >= a[i]) {
+				max=max;
+				
+			} else {
+				max=a[i];
+				index=i+1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(index);
+	}
+
+}
