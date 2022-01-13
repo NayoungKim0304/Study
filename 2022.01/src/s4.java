e142ba1c15d73bcb5e5ddfd26eba0222b5f3b791
KimNayoung <kny8451@naver.com>
2022-01-13 23:58:12 +0900

    백준 2577번

---
+++ 2022.01/src/s4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class s4 {
+
+	public static void main(String[] args) {
+
+		//백준 2577번(https://www.acmicpc.net/problem/2577)
+		//세 자연수 A,B,C가 주어질때 AXBXC를 계산한 결과에
+		//0부터 9까지 각각 몇번씩 쓰였는지 구하는 프로그램
+		
+		Scanner sc = new Scanner(System.in);
+		
+		sc.nextInt();
+		sc.nextInt();
+		sc.nextInt();
+		
+		
+	}
+
+}
