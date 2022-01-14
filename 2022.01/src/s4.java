1d79121065626319d9954911c376249c41b3f5f5
KimNayoung <kny8451@naver.com>
2022-01-14 23:41:36 +0900

    백준 2577번 숫자의 개수 구하기

--- 2022.01/src/s4.java
+++ 2022.01/src/s4.java
@@ -10,11 +10,26 @@ public class s4 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		sc.nextInt();
-		sc.nextInt();
-		sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
 		
+		//0부터 9까지 숫자의 개수를 담는 크기 10의 배열 생성
 		
-	}
+		int[] arr = new int[10];
+		int result = A*B*C;
+		
+		//마지막 자리의 수는 result를 10으로 나누었을때 나머지가 된다.
+		//그 다음 10으로 나눈 몫으로 다시 반복하면 각 숫자의 개수를 구할 수 있다.
+		//arr[0]은 result를 10으로 나눈 나머지가 0이 될때 카운트를 올린다고 생각하면
+		while(result > 0) {
+			arr[result%10]++;
+			result = result / 10;
+		}
+		
+		for(int i = 0; i< arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 
+	}
 }
