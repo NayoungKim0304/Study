6ee00832f6afb673b747044920b7cc0a4f64f5d7
KimNayoung <kny8451@naver.com>
2022-02-23 23:55:13 +0900

    다른 방법 풀이

--- Study/src/Feb/s10.java
+++ Study/src/Feb/s10.java
@@ -1,6 +1,7 @@
 package Feb;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class s10 {
 
@@ -9,24 +10,46 @@ public class s10 {
 		//백준 11653번(https://www.acmicpc.net/problem/11653)
 		//정수 N이 주어졌을때 소인수 분해하는 프로그램 작성
 		
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int N = sc.nextInt();
+//		
+//		//가장 작은 소수인 2부터 나누기 시작하여 더이상 2로 나누어 지지 않으면
+//		//다음 소수인 3으로 나누는 방식으로 작성한다.
+//		//더이상 나누어지지 않을때까지 반복한다.
+//		
+//		int i = 2;
+//		
+//		while(N >= i) {
+//			if(N%i == 0) {
+//				System.out.println(i);
+//				N /= i;
+//			}else {
+//				i++;
+//			}
+//		}
 		
-		int N = sc.nextInt();
 		
-		//가장 작은 소수인 2부터 나누기 시작하여 더이상 2로 나누어 지지 않으면
-		//다음 소수인 3으로 나누는 방식으로 작성한다.
-		//더이상 나누어지지 않을때까지 반복한다.
+		//BufferedReader 사용
 		
-		int i = 2;
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 		
-		while(N >= i) {
-			if(N%i == 0) {
-				System.out.println(i);
-				N /= i;
-			}else {
-				i++;
-			}
-		}
+		StringBuilder sb = new StringBuilder();
+////		int N = Integer.parseInt(buffer.readLine())
+//		
+//		for(int i=2; i<=Math.sqrt(N); i++) {
+//			while(N%i==0) {
+//				sb.append(i).append('\n');
+//				N/=i;
+//			}
+//		}
+//		
+//		if(N != 1) {
+//			sb.append(N);
+//			
+//		}
+//		System.out.println(sb);
+		
 	}
 
 }
