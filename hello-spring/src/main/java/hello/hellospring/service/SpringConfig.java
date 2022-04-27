6ca6183a33e44fca2c866a8b5d00aae5103df44b
kny <kny8451@naver.com>
2022-04-27 23:57:12 +0900

    순수 JDBC방식으로 db연결

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,14 +1,24 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     //빈 -> 스프링 컨테이너가 관리하는 자바객체
     // -component annotation 자동등록 사용(스프링이 확인하고 자체적으로 bean으로 등록)
     // -configuration을 이용해 bean 직접 등록
@@ -20,7 +30,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
 
-        return new MemoryMemberRepository();
     }
 }
